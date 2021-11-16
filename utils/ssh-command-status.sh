1de27a26783967406bfe2bccca85ebea7b998b22
Tim Bird <tim.bird@sony.com>
2021-11-16 14:29:40 -0700

    utils: adjust ssh-command-status.sh output
    Make the output of ssh-command-status.sh more clear.  Only show
    sshpass and ssh stderr output when in verbose mode.  Make sure
    the first line has the actual status word (OPERATIVE or INOPERATIVE)
    
    Adjust the usage to indicate optional vs. required arguments, along
    with defaults for optional args.
    
    Signed-off-by: Tim Bird <tim.bird@sony.com>

--- utils/ssh-command-status.sh
+++ utils/ssh-command-status.sh
@@ -2,21 +2,40 @@
 # the target board.
 #
 # Usage: command_status.sh [-h|--help] -u {user_name} -p {password} -a    {ipaddr} -c {command}
-
+#
+# NOTES: if you get an error message:
+#    "Could not create directory '/var/www/.ssh'"
+# Do the following as root:
+#   mkdir -p /var/www/.ssh ; \
+#     chown www-data.www-data /var/www/.ssh \
+#     chmod 700 /var/www/.ssh
+#
 
 usage() {
     cat <<HERE
-Usage: command_status.sh [-h|--help] -u {user_name} -p {password} -a {ipaddr} -c {command}
+Usage: command_status.sh [options] -u {user_name} -p {password} -a {ipaddr}
 
-It runs a command on target board to check command execution status of the target board
+It runs a command on target board to check command execution status
+of the target board.  The first line of output will be the single
+word "OPERATIVE" or "INOPERATIVE", depending on whether the
+command could be run on the board.  Use '-v' to see verbose execution
+and error reporting information, in case of problems.
 
 Options:
  -h|--help          Show usage help
+ -v                 Show verbose output
  -u {user_name}     Specify the user name of target board
  -p {password}      Specify the password of target board.
- -a {ipaddr}        Specify the ip address of the target board
+ -a {ipaddr}        Specify the ip address or host name of the target board
  -c {command}       Specify the command to be executed on the target board.
- -t {timeout}       Specify the ssh ConnectTimeout (default=2)
+                    Defaults to 'true' if not specified.
+ -t {timeout}       Specify the ssh ConnectTimeout in seconds (default=2)
+
+NOTE: if you get the error message:
+   "Could not create directory '/var/www/.ssh'"
+you can fix it by doing the following as root:
+   cd /var/www ; mkdir -p .ssh ; chown www-data.www-data .ssh ; chmod 700 .ssh
+
 HERE
     exit 1
 }
@@ -62,34 +81,47 @@ done
 
 SSH_ARGS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=ERROR -o ConnectTimeout=$TIMEOUT"
 
-sshpass -p $PASSWORD ssh $SSH_ARGS $USER@$IPADDR $CMD 2>&1
+stderr_outfile=$(mktemp)
+sshpass -p $PASSWORD ssh $SSH_ARGS $USER@$IPADDR $CMD 2>$stderr_outfile
 status=$?
 
 #echo "$verbose"
 
-if [ "$verbose" = "true" ] ; then
-	if [ $status -eq 255 ] ; then
-		echo "Exit status out of range"
-	elif [ $status -eq 126 ] ; then
-		echo "Command invoked cannot execute "
-	elif [ $status -eq 127 ] ; then
-		echo "Command not found"
-	elif [ $status -eq 128 ] ; then
-		echo "Invalid argument to exit command "
-	elif [ $status -eq 130 ] ; then
-		echo "Bash script terminated by Control-C ----->Script terminated "
-	elif [ $status -eq 0 ] ; then
-		echo "OPERATIVE -----> command succesfull"
-
-	else
-		echo "NON-OPERATIVE ------> Command failed due to unknown reason"
-
-	fi
+# first output the status
+if [ "$status" = "0" ] ; then
+	echo "OPERATIVE"
 else
-	if [ "$status" = "0" ] ; then
-		echo "OPERATIVE"
+    echo "INOPERATIVE"
+fi
 
-	else
-		echo "INOPERATIVE"
-	fi
+# second, output more diagnosis, if requested
+if [ "$verbose" = "true" ] ; then
+    echo -n "  "
+    if [ $status -eq 0 ] ; then
+        echo "Command was succesful"
+    elif [ $status -eq 255 ] ; then
+        echo "Exit status out of range"
+    elif [ $status -eq 126 ] ; then
+        echo "Command invoked cannot execute"
+    elif [ $status -eq 127 ] ; then
+        echo "Command not found"
+    elif [ $status -eq 128 ] ; then
+        echo "Invalid argument to exit command"
+    elif [ $status -eq 130 ] ; then
+        echo "Bash script terminated by Control-C -->Script terminated"
+    else
+        echo "Command failed due to unknown reason"
+    fi
+    echo "  Executed command: sshpass -p $PASSWORD ssh $SSH_ARGS $USER@$IPADDR $CMD"
+    echo "  Command exit code=$status"
+    if [ -s "$stderr_outfile" ] ; then
+        echo -n "  STDERR from command: "
+        cat $stderr_outfile
+        echo
+    fi
+fi
+
+# remove stderr data temp file
+if [ -f "$stderr_outfile" ] ; then
+    rm $stderr_outfile
 fi
